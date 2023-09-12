e83f615f070395b839f6807abd231e44dfb37d8d
Maura Hunter <Maura Hunter@10.0.0.146>
2023-09-12 17:32:23 -0400

    I made the polygon a lot more wider. I changed the different corners

--- extrudePolygon.groovy
+++ extrudePolygon.groovy
@@ -2,8 +2,8 @@
 //create an extruded polygon
 CSG polygon = Extrude.points(new Vector3d(0, 0, 40),// This is the  extrusion depth
                 new Vector3d(0,0),// All values after this are the points in the polygon
-                new Vector3d(80,0),// Bottom right corner
-                new Vector3d(60,40),// upper right corner
+                new Vector3d(135,0),// Bottom right corner
+                new Vector3d(100,70),// upper right corner
                 new Vector3d(20,40)// upper left corner
         );
 return polygon
